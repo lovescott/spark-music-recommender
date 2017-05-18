f49dfaabf0f6564450c9fe9e0666a101fd79f386
Scott Love <scott.love@gmail.com>
2017-05-18 13:58:34 -0400

    id int32 check

--- build.sbt
+++ build.sbt
@@ -4,6 +4,6 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.0"
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.1.0"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.1"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.1.1"
     
--- src/main/scala/MusicRecommender.scala
+++ src/main/scala/MusicRecommender.scala
@@ -1,24 +1,38 @@
 
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql._
-import org.apache.spark.broadcast._
+import scala.collection.Map
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+import org.apache.spark.broadcast.Broadcast
+//import org.apache.spark.ml.recommendation.{ALS, ALSModel}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.log4j._
 
 
 object MusicRecommender {
 
   def main(args: Array[String]): Unit ={
 
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    //confiure spark session with needed memory and other
     val spark = SparkSession
       .builder()
       .appName("MusicRecommender")
-      .master("local")
+      .master("local[*]")
       .getOrCreate()
-
+    spark.conf.set("spark.driver.memory", "4g")
     import spark.implicits._
 
-    val rawArtistData = spark.read.textFile("data/user_artist_data.txt")
+    val rawArtistData = spark.read.textFile("data/user_artist_data.txt").repartition(8)
 
-    rawArtistData.take(5).foreach(println)
+    val userArtistDF = rawArtistData.map { line =>
+      val Array(user, artist, _*) = line.split(' ')
+      (user.toInt, artist.toInt)
+    }.toDF("user", "artist")
+
+    userArtistDF.agg(min("user"), max("user"), min("artist"), max("artist")).show()
+
 
   }
 }
